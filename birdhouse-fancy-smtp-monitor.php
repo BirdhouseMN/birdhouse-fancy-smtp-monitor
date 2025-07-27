410afd00fd9fd5da164620550a86eaea2a693675
BirdhouseMN <birdie@birdhousewebsites.com>
2025-07-27 16:26:19 -0500

    v1.0.6

--- birdhouse-fancy-smtp-monitor.php
+++ birdhouse-fancy-smtp-monitor.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Birdhouse Fancy SMTP Monitor
  * Description: Responds to remote SMTP status checks from a central manager site.
- * Version: 1.0.5
+ * Version: 1.0.6
  * Author: Birdhouse Web Design
  * License: GPL2
  */
